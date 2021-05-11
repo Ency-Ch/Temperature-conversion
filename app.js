984258afa4c37a284a5b2df7d4e5e8b16a6d79ff
ency ch <encee07@yahoo.co.uk>
2021-05-11 17:49:19 +0100

    output units added

--- app.js
+++ app.js
@@ -22,11 +22,11 @@ function convertTemp(e){
     const tempInput = document.getElementById('temp-input')
     const tempInputUnits = document.getElementById('input-options')
     const tempOutput = document.getElementById('temp-output')
-    const tempOutputUnits = document.getElementById('output-options')
+    const tempOutputUnits = document.getElementById('output-options');
 
     const input = parseInt(tempInput.value);
     const Units = tempInputUnits.value;
-    let OutputUnits = tempOutputUnits.value;
+    let OutputUnits = tempOutputUnits.nodeValue;
 
     const Output1 = (input - 32) * (5/9);
     const Output2 = (input * 9/5) + 32;
@@ -35,7 +35,8 @@ function convertTemp(e){
     if (Units === '℉'){
        (tempOutput.value) = Output1;
 
-       OutputUnits = '℃'
+       OutputUnits ='℃'
+       tempOutputUnits.value = OutputUnits
 
       
        
@@ -43,10 +44,13 @@ function convertTemp(e){
     }else{(tempOutput.value) = Output2;
 
         OutputUnits = '℉'
+        tempOutputUnits.value = OutputUnits
+
          
         }
 
-       
+      
+
 e.preventDefault()
 
 }
