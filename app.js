0c3b23e41f9e6befd953b42c5fca0dbb6a78ccb0
ency ch <encee07@yahoo.co.uk>
2021-05-13 00:23:49 +0100

    error functio added

--- app.js
+++ app.js
@@ -2,55 +2,145 @@
 
 
 const tempInput = document.getElementById('temp-input')
+
 const tempInputUnits = document.getElementById('input-options')
+
 const tempOutput = document.getElementById('temp-output')
+
 const tempOutputUnits = document.getElementById('output-options')
 
 
+document.getElementById('loading').style.display = 'none';
 
-//console.log(tempInput)
-//alert(tempInput)
+document.getElementById('conversion-form').addEventListener('submit', function(e){
 
-document.getElementById('conversion-form').addEventListener('submit', convertTemp)
+    //hide results
+    document.getElementById('temp-output').style.display = 'none'
+    document.getElementById('output-options').style.display = 'none'
+
+    //show loader 
+    document.getElementById('loading').style.display = 'block';
+
+    setTimeout(convertTemp, 2000)
+
+    e.preventDefault()
 
 
-function convertTemp(e){
+})
+
+
+
+
+
+function convertTemp(){
+
     console.log('calculating')
-    
-
 
     const tempInput = document.getElementById('temp-input')
     const tempInputUnits = document.getElementById('input-options')
     const tempOutput = document.getElementById('temp-output')
     const tempOutputUnits = document.getElementById('output-options');
 
-    const input = parseInt(tempInput.value);
+    const input = parseFloat(tempInput.value);
     const Units = tempInputUnits.value;
     let OutputUnits = tempOutputUnits.nodeValue;
 
     const Output1 = (input - 32) * (5/9);
     const Output2 = (input * 9/5) + 32;
     
+    
 
-    if (Units === '℉'){
-       (tempOutput.value) = Output1.toFixed(2);
-
-       OutputUnits ='℃'
-       tempOutputUnits.value = OutputUnits
-
-      
-       
-
-    }else{(tempOutput.value) = Output2.toFixed(2);
-
-        OutputUnits = '℉'
-        tempOutputUnits.value = OutputUnits
-
+        if (isFinite(input) && Units === '℉'){
+            (tempOutput.value) = Output1.toFixed(2);
+     
+            OutputUnits ='℃'
+            tempOutputUnits.value = OutputUnits
+            //show results
+            document.getElementById('temp-output').style.display = 'inline-grid'
+            document.getElementById('output-options').style.display = 'inline-grid'
+            document.getElementById('loading').style.display = 'none'
+     
+           
+            
+     
+         }else if (isFinite(input) && Units === '℃') {
          
-        }
+         (tempOutput.value) = Output2.toFixed(2);
+     
+             OutputUnits = '℉'
+             tempOutputUnits.value = OutputUnits
+             //show results 
+             document.getElementById('temp-output').style.display = 'inline-grid'
+             document.getElementById('output-options').style.display = 'inline-grid'
+             document.getElementById('loading').style.display = 'none'
+             console.log(input)
+         }
 
-      
+    
 
-e.preventDefault()
+   else{
+        showError('please enter your temperature as a number')
+
+        
+
+    }
+
+ 
 
 }
+
+function showError(error){
+    //hide result 
+    document.getElementById('temp-output').style.display = 'none';
+    document.getElementById('output-options').style.display = 'none';
+    //hide the loader
+    document.getElementById('loading').style.display = 'none';
+    //create a div 
+
+    //const errorDiv = document.createElement('div');
+    //add class
+    //errorDiv.className = 'alert';
+
+
+
+    var tag = document.createElement('p');
+    //tag.setAttribute('id', 'paragraph')
+    var text = document.createTextNode(error);
+    tag.appendChild(text);
+    var element = document.querySelector('.container');
+    element.appendChild(text);
+
+
+    //get elements
+    //const container = document.querySelector('#outside-border')
+    //const heading = document.querySelector('.heading')
+
+    //create text node and append to div 
+    //errorDiv.appendChild(document.createTextNode(error))
+
+    //insert error before heading 
+    //container.insertBefore(errorDiv, heading)
+    
+    
+
+    
+    setTimeout(clearError, 2000)
+    
+
+    //console.log(error)
+}
+
+
+
+
+function clearError(){
+    let val = document.querySelector('.container').childNodes[9]
+   // var myobj = document.getElementById("paragraph");
+   // myobj.remove()
+   
+    //addEventListener('DOMContentLoaded', element2.remove());
+    val.remove()
+}
+
+
+
